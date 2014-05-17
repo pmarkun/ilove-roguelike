92fc216fc202ccb1eedaf5df873d6179e6e6007e
Pedro Markun <pedro@markun.com.br>
2014-05-17 02:11:11 -0300

    modificando para tiles (16,16) e usando keypressed ao inves do update

--- roguelike/main.lua
+++ roguelike/main.lua
@@ -1,27 +1,35 @@
 function love.load() -- load no inicio do jogo
+	love.keyboard.setKeyRepeat(true)
+	
+	tile = 16
+
+	map = {}
+	map.width = 40
+	map.height = 30
+
 	hero = {} -- new table for the hero
- 	hero.x = 300    -- x,y coordinates of the hero
- 	hero.y = 450
- 	hero.speed = 100
+ 	hero.x = 16    -- x,y coordinates of the hero
+ 	hero.y = 16
+ 	hero.speed = 1
 end
 
-function love.update(dt) -- loop principal do jogo // dt é o tempo passado
-	if love.keyboard.isDown("left") then
-  		hero.x = hero.x - hero.speed*dt
- 	elseif love.keyboard.isDown("right") then
-   		hero.x = hero.x + hero.speed*dt
- 	elseif love.keyboard.isDown("up") then
- 		hero.y = hero.y - hero.speed*dt
- 	elseif love.keyboard.isDown("down") then
- 		hero.y = hero.y + hero.speed*dt
- 	end
+function love.keypressed(key) -- loop principal do jogo // dt é o tempo passado
+	if key == "left" and hero.x-hero.speed*tile >= 0 then
+  		hero.x = hero.x - hero.speed*tile
+ 	elseif key == "right" and hero.x+hero.speed*tile <= map.width*tile-tile then
+   		hero.x = hero.x + hero.speed*tile
+ 	elseif key == "up" and hero.y-hero.speed*tile >= 0 then
+ 		hero.y = hero.y - hero.speed*tile
+ 	elseif key == "down" and hero.y+hero.speed*tile <= map.height*tile-tile then
+ 		hero.y = hero.y + hero.speed*tile
+	end
 end
 
 function love.draw() --desenha o que precisar ser desenhado
 	-- let's draw some ground
-	love.graphics.setColor(100,100,100,255) -- R,G,B, Luz
-	love.graphics.rectangle("fill", 0, 0, 640, 480) -- mode,x,y,w,h
+	love.graphics.setColor(100,100,100) -- R,G,B, Luz
+	love.graphics.rectangle("fill", 0, 0, map.width*tile, map.height*tile) -- mode,x,y,w,h
 	-- let's draw our hero
-	love.graphics.setColor(255,255,0,255)
+	love.graphics.setColor(255,255,0)
 	love.graphics.rectangle("fill", hero.x,hero.y, 16, 16)
 end
